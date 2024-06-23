e0f15a4e251f68a75b90ed2f8f73a87c5269c003
mareme <mariamathiaw92@gmail.com>
2024-06-23 09:39:41 +0000

    Implémentation des routes et des controllers pour l'authentification

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IdeeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\CategorieController;
 
 Route::get('/', function () {
@@ -22,3 +25,11 @@ Route::get('/idees', [IdeeController::class, 'index'])->name('idees.index');
 Route::get('/idees/{idee}/edit', [IdeeController::class, 'edit'])->name('idees.edit');
 Route::put('/idees/{idee}', [IdeeController::class, 'update'])->name('idees.update');
 Route::delete('/idees/{idee}', [IdeeController::class, 'destroy'])->name('idees.destroy');
+
+//Autentification
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LogoutController::class, 'logout'])->name('logout');
+
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
